8a90f20dadf951d99ac4ca08aea7bd66eebc3b11
James McDermott <james.mcdermott7@mail.dcu.ie>
2019-07-19 18:12:15 +0000

    add haveibeenpwned functionality - closes #5

--- bot.js
+++ bot.js
@@ -14,6 +14,11 @@ bot.on('message', (receivedMessage) => {
     }
 })
 
+function boldify(contents) {
+	return "**" + contents + "**"
+}
+
+
 function processCommand(receivedMessage) {
     let fullCommand = receivedMessage.content.substr(1)
     let splitCommand = fullCommand.split(" ")
@@ -27,8 +32,10 @@ function processCommand(receivedMessage) {
         helpCommand(arguments, receivedMessage)
     } else if (primaryCommand == "isitup") {
         isItUpCommand(arguments, receivedMessage)
+    } else if (primaryCommand == "pwned") {
+        pwnedCommand(arguments, receivedMessage)
     } else {
-        receivedMessage.channel.send("I don't understand the command. Try `!help` or `!multiply`")
+        receivedMessage.channel.send("I don't understand the command. Try `!help [command]`")
     }
 }
 
@@ -38,9 +45,11 @@ function helpCommand(arguments, receivedMessage) {
     } else if (arguments.length == 1) {
 	    if (arguments == "isitup") {
 	    	receivedMessage.channel.send("_isitup_ - check if a site is up or down.\nExample: `!isitup redbrick.dcu.ie`")
-	    }
+	    } else if (arguments == "pwned") {
+	    	receivedMessage.channel.send("_pwned_ - check if an email has been pwned .\nExample: `!pwned bertie@redbrick.dcu.ie`")
+            }
     } else {
-        receivedMessage.channel.send("Here is the list of brickbot commands:\n • isitup\n • help")
+        receivedMessage.channel.send("Here is the list of brickbot commands:\n • isitup\n • pwned\n • help")
     }
 }
 
@@ -54,9 +63,30 @@ function isItUpCommand(arguments, receivedMessage) {
             url:     'https://faas.jamesmcdermott.ie/function/isitup',
             body:    arguments
         }, function(error, response, body) {
-            receivedMessage.channel.send(body)
+            receivedMessage.channel.send(boldify(body))
         });
     }
+}
+
+
+function pwnedCommand(arguments, receivedMessage) {
+    email = arguments
+    if (arguments.length == 0) {
+	receivedMessage.channel.send("No email supplied. Try `!pwned bertie@redbrick.dcu.ie`")
+        return
+    } else if (arguments.length > 0) {
+        request.post({
+            url:     'https://faas.jamesmcdermott.ie/function/haveibeenpwned',
+            body:    arguments
+        }, function(error, response, body) {
+		n = JSON.parse(body).found
+		if (n == 0) { 
+                	receivedMessage.channel.send(boldify(email + " has not been pwned"))
+            	} else {
+                	receivedMessage.channel.send(boldify(email + " has been pwned"))
+            	}
+        });
+    } 
 }
 
 bot_secret_token = fs.readFileSync("/tmp/brickbot.token", "utf-8").replace(/\n$/, '')
