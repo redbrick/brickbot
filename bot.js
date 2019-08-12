dd1e0410fac9c7d033c89c4d3216b0a1ac778a71
James McDermott <james.mcdermott7@mail.dcu.ie>
2019-08-12 15:36:30 +0000

    swap blocky control flow for switch statements

--- bot.js
+++ bot.js
@@ -4,6 +4,12 @@ var fs = require("fs");
 
 var bot = new Discord.Client();
 
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// LISTEN AND PARSE FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 bot.on("message", (receivedMessage) => {
 	if (receivedMessage.author == bot.user) { 
 		return;
@@ -22,53 +28,81 @@ function processCommand(receivedMessage) {
 	console.log("Command received: " + primaryCommand);
 	console.log("args: " + args);
 
-	if (primaryCommand == "help") {
-		helpCommand(args, receivedMessage);
-	} else if (primaryCommand == "bus") {
-		busCommand(args, receivedMessage);
-	} else if (primaryCommand == "coinflip") {
-		coinflipCommand(args, receivedMessage);
-	} else if (primaryCommand == "wiki") {
-		wikiCommand(args, receivedMessage);
-	} else if (primaryCommand == "isitup") {
-		isItUpCommand(args, receivedMessage);
-	} else if (primaryCommand == "luas") {
-		luasCommand(args, receivedMessage);
-	} else if (primaryCommand == "nslookup") {
-		nslookupCommand(args, receivedMessage);
-	} else if (primaryCommand == "pwgen") {
-		pwgenCommand(args, receivedMessage);
-	} else if (primaryCommand == "pwned") {
-		pwnedCommand(args, receivedMessage);
-	} else if (primaryCommand == "ssl") {
-		sslCommand(args, receivedMessage);
-	} else {
-		receivedMessage.channel.send("I don't understand the command. Try `!help [command]`");
+        switch (primaryCommand) {
+            case "help":
+                helpCommand(args, receivedMessage);
+                break;
+            case "bus":
+                busCommand(args, receivedMessage);
+                break;
+            case "coinflip":
+                coinflipCommand(args, receivedMessage);
+                break;
+            case "isitup":
+                isItUpCommand(args, receivedMessage);
+                break;
+            case "luas":
+                luasCommand(args, receivedMessage);
+                break;
+            case "nslookup":
+                nslookupCommand(args, receivedMessage);
+                break;
+            case "pwgen":
+                pwgenCommand(args, receivedMessage);
+                break;
+            case "pwned":
+                pwnedCommand(args, receivedMessage);
+                break;
+            case "ssl":
+                sslCommand(args, receivedMessage);
+                break;
+            case "wiki":
+                wikiCommand(args, receivedMessage);
+                break;
+            default:
+                receivedMessage.channel.send("I don't understand the command. Try `!help [command]`");
 	}
 }
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// BOT COMMAND FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function helpCommand(args, receivedMessage) {
 	if (args.length > 1) {
 		receivedMessage.channel.send("Please specify one single command. Try `!help [command]`");
 	} else if (args.length == 1) {
-            if (args == "bus") {
-                receivedMessage.channel.send(codify("bus - check the schedule of a Dublin Bus stop.\n\nExample: '!bus 1635'\n\nThe nearest bus stops to DCU are 7516 (The Helix) and 37 (Ballymun Road)."));
-            } else if (args == "coinflip") {
-                receivedMessage.channel.send(codify("coinflip - toss a coin.\n\nExample: '!coinflip'"));
-            } else if (args == "isitup") {
-                receivedMessage.channel.send(codify("isitup - check if a site is up or down.\n\nExample: '!isitup redbrick.dcu.ie'"));
-            } else if (args == "luas") {
-                receivedMessage.channel.send(codify("luas - check the schedule of a Luas stop.\n\nExample: '!luas harcourt'"));
-            } else if (args == "nslookup") {
-                receivedMessage.channel.send(codify("nslookup - uses nslookup to return any IP address info on domains.\n\nExample: '!nslookup redbrick.dcu.ie'"));
-            } else if (args == "pwgen") {
-                receivedMessage.channel.send(codify("pwgen - uses pwgen to generate a password and privately send it to you.\n\nExample: '!pwgen'`"));
-            } else if (args == "pwned") {
-                receivedMessage.channel.send(codify("pwned - check if an email has been pwned.\n\nExample: '!pwned bertie@redbrick.dcu.ie'"));
-            } else if (args == "ssl") {
-                receivedMessage.channel.send(codify("ssl - check the certificate info of a website.\n\nExample: '!ssl redbrick.dcu.ie'"));
-            } else if (args == "wiki") {
-                receivedMessage.channel.send(codify("wiki - return a random page from wiki.redbrick.dcu.ie.\n\nExample: '!wiki'"));
+            switch (args) {
+                case "bus":
+                    receivedMessage.channel.send(codify(`bus - check the schedule of a Dublin Bus stop.\n\nExample: '!bus 1635'\n\n
+                                                        The nearest bus stops to DCU are 7516 (The Helix) and 37 (Ballymun Road).`));
+                break;
+                case "coinflip":
+                    receivedMessage.channel.send(codify("coinflip - toss a coin.\n\nExample: '!coinflip'"));
+                    break;
+                case "isitup":
+                    receivedMessage.channel.send(codify("isitup - check if a site is up or down.\n\nExample: '!isitup redbrick.dcu.ie'"));
+                    break;
+                case "luas":
+                    receivedMessage.channel.send(codify("luas - check the schedule of a Luas stop.\n\nExample: '!luas harcourt'"));
+                    break;
+                case "nslookup":
+                    receivedMessage.channel.send(codify("nslookup - uses nslookup to return any IP address info on domains.\n\nExample: '!nslookup redbrick.dcu.ie'"));
+                    break;
+                case "pwgen":
+                    receivedMessage.channel.send(codify("pwgen - uses pwgen to generate a password and privately send it to you.\n\nExample: '!pwgen'`"));
+                    break;
+                case "pwned":
+                    receivedMessage.channel.send(codify("pwned - check if an email has been pwned.\n\nExample: '!pwned bertie@redbrick.dcu.ie'"));
+                    break;
+                case "ssl":
+                    receivedMessage.channel.send(codify("ssl - check the certificate info of a website.\n\nExample: '!ssl redbrick.dcu.ie'"));
+                    break;
+                case "wiki":
+                    receivedMessage.channel.send(codify("wiki - return a random page from wiki.redbrick.dcu.ie.\n\nExample: '!wiki'"));
+                    break;
             }
         } else {
             receivedMessage.channel.send(codify("Here is the list of brickbot commands:\n • bus \n • coinflip\n • isitup\n • luas\n • nslookup\n • pwgen\n • pwned\n • ssl\n • help\n • wiki"));
@@ -77,7 +111,7 @@ function helpCommand(args, receivedMessage) {
 
 function busCommand(args, receivedMessage) {
 	if (args.length == 0) {
-		receivedMessage.channel.send("No stop supplied. Try `!bus 7571`");
+		argumentsUsedExample(receivedMessage, "stop", "!bus 7571");
 		return;
 	}
 	else if (args.length > 0) {
@@ -107,7 +141,7 @@ function coinflipCommand(args, receivedMessage) {
 
 function isItUpCommand(args, receivedMessage) {
 	if (args.length == 0) {
-		receivedMessage.channel.send("No URL supplied. Try `!isitup redbrick.dcu.ie`");
+		argumentsUsedExample(receivedMessage, "URL", "!isitup redbrick.dcu.ie");
 		return;
 	}
 	else if (args.length > 0) {
@@ -122,7 +156,7 @@ function isItUpCommand(args, receivedMessage) {
 
 function luasCommand(args, receivedMessage) {
 	if (args.length == 0) {
-		receivedMessage.channel.send("No stop supplied. Try `!luas harcourt`");
+		argumentsUsedExample(receivedMessage, "stop", "!luas harcourt");
 		return;
 	}
 	else if (args.length > 0) {
@@ -138,7 +172,7 @@ function luasCommand(args, receivedMessage) {
 
 function nslookupCommand(args, receivedMessage) {
 	if (args.length == 0) {
-		receivedMessage.channel.send("No URL supplied. Try `!nslookup redbrick.dcu.ie`");
+		argumentsUsedExample(receivedMessage, "URL", "!nslookup redbrick.dcu.ie");
 		return;
 	}
 	else if (args.length > 0) {
@@ -162,7 +196,7 @@ function pwgenCommand(args, receivedMessage) {
 function pwnedCommand(args, receivedMessage) {
 	var email = args;
 	if (args.length == 0) {
-		receivedMessage.channel.send("No email supplied. Try `!pwned bertie@redbrick.dcu.ie`");
+		argumentsUsedExample(receivedMessage, "email", "!pwned bertie@redbrick.dcu.ie");
 		return;
 	} else if (args.length > 0) {
 		request.post({
@@ -181,7 +215,7 @@ function pwnedCommand(args, receivedMessage) {
 
 function sslCommand(args, receivedMessage) {
 	if (args.length == 0) {
-		receivedMessage.channel.send("No URL supplied. Try `!ssl redbrick.dcu.ie`");
+		argumentsUsedExample(receivedMessage, "URL", "!ssl redbrick.dcu.ie");
 		return;
 	}
 	else if (args.length > 0) {
@@ -196,7 +230,7 @@ function sslCommand(args, receivedMessage) {
 
 function wikiCommand(args, receivedMessage) {
 	if (args.length > 0) {
-		receivedMessage.channel.send("Too many arguments. Try '!wiki' on it's own (New features on the way!)");
+                noArgumentsUsedExample(receivedMessage, "!wiki");
 		return;
 	}
 	else if (args.length == 0) {
@@ -213,12 +247,20 @@ bot.login(bot_secret_token);
 
 
 
-////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // HELPER FUNCTIONS
-///////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function codify(contents) {
 	return "```" + contents + "```";
+}
+
+function argumentsUsedExample(receivedMessage, required, command) {
+    receivedMessage.channel.send("No " + required + " supplied. " + "Try `" + command + "`");
+}
+
+function noArgumentsUsedExample(receivedMessage, command) {
+    receivedMessage.channel.send("Too many arguments supplied. Try `" + command + "`");
 }
 
 function busParseTime(arr) {
