a8c4f30fd0d22b5f1e362a6bf1a2e5796a8a6350
James McDermott <james.mcdermott7@mail.dcu.ie>
2019-10-09 00:54:06 +0100

    fix verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,53 +1,43 @@
 var fs = require("fs");
-var ldap = require('ldapjs');
+var ldap = require("ldapjs");
 
 var helpers = require("../helpers/helpers.js");
 
-var client = ldap.createClient({
-  url: 'ldaps://ldap.internal:389'
+const ldapClient = ldap.createClient({
+  url: "ldap://ldap.internal"
 });
 
-const ldapSecret = fs.readFileSync('/etc/ldap.secret');
-const sanitizer = /[^a-zA-Z0-9]/gi;
-
-let verifyCommand = (bot, args, receivedMessage) => {
-  if (args.length == 0) {
-    receivedMessage.channel
-      .send("!verify your-redbrick-username your-dcu-email-address");
-    return;
-  }
-  client.bind('cn=root,ou=ldap,o=redbrick', ldapSecret,function(err) {
-    client.compare(
-      `uid=${args[0].replace(sanitizer, '')}`,
-      'altmail',
-      arg[1].replace(sanitizer, ''),
-      (err, matched) => {
-        if (!err) {
-          console.log(matched);
-          const serverNumber = fs
-            .readFileSync("/tmp/discord.server.number", "utf-8")
-            .replace(/\n$/, "");
-          const memberRole = fs
-            .readFileSync("/tmp/discord.member.role", "utf-8")
-            .replace(/\n$/, "");
-          bot.guilds
-            .get(serverNumber)
-            .members
-            .get(receivedMessage.author.id).addRole(memberRole);
-          receivedMessage.channel
-            .send(helpers.embedify(bot, "Verified!"));
-        } else {
-          receivedMessage.channel
-            .send(helpers.embedify(
-              bot,
-              "username and email address do not match")
-            );
-        }
-      }
-    );
-  });
-}
+const ldapSecret = fs.readFileSync("/etc/ldap.secret", "utf-8");
 
 module.exports = {
-  verifyCommand
+    verifyCommand: function(bot, args, receivedMessage) {
+        if (args.length == 0) {
+            receivedMessage.channel.send("!verify your-redbrick-username your-dcu-email-address");
+            return;
+        }
+
+        const searchOptions = {
+            scope: "sub",
+            filter: (`uid=${args[0]}`)
+        };
+
+        ldapClient.bind("cn=root,ou=ldap,o=redbrick", ldapSecret, function(err) {
+            if (err) {
+                receivedMessage.channel.send(helpers.embedify(bot,"Could not connect to Redbrick LDAP server, please contact a member of Committee."));
+            } else {
+                ldapClient.search("o=redbrick", searchOptions, function(error, result) {
+                    result.on("searchEntry", function(entry) {
+                        if (entry.object.altmail == args[1]) {
+                            const serverNumber = fs.readFileSync("/etc/discord.server.number", "utf-8").replace(/\n$/, "");
+                            const memberRole = fs.readFileSync("/etc/discord.member.role", "utf-8").replace(/\n$/, "");
+                            bot.guilds.get(serverNumber).members.get(receivedMessage.author.id).addRole(memberRole);
+                            receivedMessage.channel.send(helpers.embedify(bot, "Verified!"));
+                        } else {
+                            receivedMessage.channel.send(helpers.embedify(bot,"Username and Email Address do not match."));
+                        }
+                    });
+                });    
+            }
+        });
+    }
 };
